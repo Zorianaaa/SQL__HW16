085e5bdcc3a5ef0f51524c46eeab6d2711637c15
Zoriana Zhuk <zoriana.zh08@gmail.com>
2024-10-08 00:49:23 +0300

    HomeWork16

---
+++ SQL_library_HW16/LibraryDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Entity;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace SQL_library_HW16
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext()
+            : base(ConfigurationManager.ConnectionStrings["OracleDbContext"].ConnectionString)
+        {
+        }
+
+        public DbSet<Librarian> Librarians { get; set; }
+        public DbSet<Reader> Readers { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Loan>()
+                .HasRequired(l => l.Reader)
+                .WithMany()
+                .HasForeignKey(l => l.ReaderId);
+
+            modelBuilder.Entity<Loan>()
+                .HasRequired(l => l.Book)
+                .WithMany()
+                .HasForeignKey(l => l.BookId);
+        }
+    }
+}
---
+++ SQL_library_HW16/Loan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SQL_library_HW16
+{
+        public class Loan
+        {
+            public int Id { get; set; }
+            public int ReaderId { get; set; }
+            public int BookId { get; set; }
+            public DateTime BorrowedDate { get; set; }
+            public DateTime? ReturnDate { get; set; }
+            public DateTime DueDate { get; set; }
+
+            public Reader Reader { get; set; }
+            public Book Book { get; set; }
+        }
+}
